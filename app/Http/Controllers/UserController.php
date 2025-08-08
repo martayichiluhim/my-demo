63b6277c13f045ce371d6aa7bb43f541a8dc0e36
Marta Mesfin <martayichiluhim29@gmail.com>
2025-08-08 22:22:27 +0300

    Initial commit - CRUD project

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'name' => ['required', 'min:4', 'max:10'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:6', 'max:30']
+        ]);
+        return 'Hello from our controller';
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+  <div style="border: 3px solid black;">
+    <h2>Register</h2>
+    <form action="/register" method="POST">
+        @csrf
+        <input type="text" name="name" placeholder="Name"><br><br>
+        <input type="email" name="email" placeholder="Email"><br><br>
+        <input type="password" name="password" placeholder="Password"><br><br>
+        <button type="submit">Register</button>
+    </form>
+  </div>  
+</body>
+</html>
